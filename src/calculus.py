355bc5a79d658179bb29761ff99c056d443606d6
AdamLassiter <adam.lassiter@hotmail.co.uk>
2017-12-08 14:02:49 +0000

    Implemented basic reduction rules

--- src/calculus.py
+++ src/calculus.py
@@ -1,11 +1,32 @@
 #! /usr/bin/env python3
 
 class Agent(object):
-    pass
+    
+    def reduce(self):
+        raise NotImplementedError
 
 
-class Name(str):
-    pass
+class Name(object):
+    
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.fusions = {self}
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        assert isinstance(other, type(self))
+        return self in other.fusions
+    
+    def fuse_into(self, other):
+        assert isinstance(other, type(self))
+        self.fusions |= {other}
+        other.fusions |= {self}
+        self.name = other.name
 
 
 class Solo(Agent):
@@ -15,12 +36,16 @@ class Solo(Agent):
             assert isinstance(name, Name)
         self.subject = subject
         self.objects = objects
+        self.arity = len(objects)
 
 
 class Input(Solo):
 
     def __str__(self) -> str:
         return '%s ' % self.subject + ''.join(map(str, self.objects))
+    
+    def reduce(self):
+        return type(self)(self.subject, self.objects)
 
 
 class Output(Solo):
@@ -28,31 +53,68 @@ class Output(Solo):
     def __str__(self) -> str:
         return '\u0305%s ' % self.subject + ''.join(map(str, self.objects))
 
+    def reduce(self):
+        return type(self)(self.subject, self.objects)
+
 
 class Inaction(Agent):
 
     def __str__(self) -> str:
         return '0'
 
+    def reduce(self):
+        return type(self)()
+
 
 class Composition(Agent):
 
-    def __init__(self, left: Agent, right: Agent) -> None:
-        self.left = left
-        self.right = right
+    def __init__(self, *agents) -> None:
+        for agent in agents:
+            assert isinstance(agent, Agent)
+        self.agents = set(agents)
 
     def __str__(self) -> str:
-        return '(%s | %s)' % (self.left, self.right)
+        return '(%s)' % (' | '.join(map(str, self.agents)))
+
+    def reduce(self):
+        agents = set()
+        # (a | b) | c == a | (b | c)
+        for agent in map(lambda x: x.reduce(), self.agents):
+            if isinstance(agent, Composition):
+                agents |= agent.agents
+            else:
+                agents |= {agent}
+        # (x)(̅u x | u y | P) == P{y / x}
+        for iagent in set(filter(lambda x: isinstance(x, Input), agents)):
+            for oagent in set(filter(lambda x: isinstance(x, Output), agents)):
+                if iagent.subject == oagent.subject \
+                and iagent.arity == oagent.arity:
+                    for iobject, oobject in zip(iagent.objects, oagent.objects):
+                        oobject.fuse_into(iobject)
+                    agents.remove(iagent)
+                    agents.remove(oagent)
+        return type(self)(*agents)
 
 
 class Scope(Agent):
 
-    def __init__(self, binding: Name, agent: Agent) -> None:
-        self.binding = binding
+    def __init__(self, bindings: set, agent: Agent) -> None:
+        for binding in bindings:
+            assert isinstance(binding, Name)
+        self.bindings = bindings
         self.agent = agent
 
     def __str__(self) -> str:
-        return '(%s)(%s)' % (self.binding, self.agent)
+        return '(%s)(%s)' % (''.join(map(str, self.bindings)), self.agent)
+
+    def reduce(self):
+        bindings = self.bindings
+        agent = self.agent.reduce()
+        # (x)(y)(P) == (xy)(P)
+        if isinstance(agent, Scope):
+            bindings |= agent.bindings
+            agent = agent.agent
+        return type(self)(bindings, agent)
 
 
 class Replication(Agent):
@@ -63,8 +125,21 @@ class Replication(Agent):
     def __str__(self) -> str:
         return '!%s' % self.agent
 
+    def reduce(self):
+        agent = self.agent.reduce()
+        # !(!(P)) == !(P)
+        if isinstance(agent, Replication):
+            agent = agent.agent
+        return type(self)(agent)
+
 
 if __name__ == '__main__':
-    expr = Replication(Composition(Composition(Input(Name('x'), (Name('y'),)), Inaction()),
-                                   Scope(Name('z'), Output(Name('z'), (Name('x'),)))))
-    print(expr)
+    x, y, z = Name('x'), Name('y'), Name('z')
+    expr = Replication(Composition(Input(z, (y,)),
+                                   Output(x, (y,)),
+                                   Output(z, (x,))))
+
+    while True:
+        print(expr)
+        input()
+        expr = expr.reduce()
